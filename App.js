2ef434a84fa682f052182ea3dda3459fa5b94fea
james-lin71 <hotdev_lm@outlook.com>
2020-08-18 13:16:17 +0200

    ios splash screen

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import 'react-native-gesture-handler';
-import * as React from 'react';
+import React, { useEffect } from 'react';
+import SplashScreen from 'react-native-splash-screen';
 
 import CoreNavigation from './src/navigation/CoreNavigation';
 
 const App = () => {
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <CoreNavigation />
   );
